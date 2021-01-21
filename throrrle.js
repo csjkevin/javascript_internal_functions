fac5c1ffb87e2694b2f6eb2af5899d7809f02629
csjkevin <csjkevin@outlook.com>
2021-01-22 02:27:36 +0800

    reformat

--- throrrle.js
+++ throrrle.js
@@ -1,10 +1,10 @@
 function throttle(fn, interval) {
-	let last = Date.now();
-	return function() {
-		let now = Date now();
-		if (now - last >= interval) {
-			fn.apply(this, arguments);
-			last = Date.now();
-		}
-	}
+  let last = Date.now();
+  return function () {
+    let now = Date.now();
+    if (now - last >= interval) {
+      fn.apply(this, arguments);
+      last = Date.now();
+    }
+  };
 }
