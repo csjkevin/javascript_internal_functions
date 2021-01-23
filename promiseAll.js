8680de3d5b33f411aba8127f0905126cc75bf1b0
csjkevin <csjkevin@outlook.com>
2021-01-23 14:09:08 +0800

    correct promise.all

--- promiseAll.js
+++ promiseAll.js
@@ -1,23 +1,24 @@
-Promise.myAll = function(promises) {
-	return new Promise(function(resolve, reject) {
-		if (Array.isArray(promises)) {
-			return;
-		}
-		let resolvedNum = 0;
-		let totalNum = promises.length;
-		let resolvedValues = new Array(totalNum);
-		for (let i = 0; i < totalNum; i++) {
-			(function(i) {
-				Promise.resolve(promises[i]).then(function(res) {
-					resolvedNum++;
-					resolvedValues[i] = value;
-					if (resolvedNum === totalNum) {
-						return resolve(resolvedValues);
-					}
-				}, function(err) {
-					return reject(err);
-				})
-			})(i)
-		}
-	})
-}
+Promise.myAll = function (promises) {
+  if (!Array.isArray(promises)) {
+    return;
+  }
+  let count = 0;
+  let total = promises.length;
+  let results = new Array(total);
+  return new Promise(function (resolve, reject) {
+    for (let i = 0; i < total; i++) {
+      Promise.resolve(promises[i]).then(
+        (res) => {
+          results[i] = res;
+          count++;
+          if (count === total) {
+            resolve(resolvedValues);
+          }
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+    }
+  });
+};
